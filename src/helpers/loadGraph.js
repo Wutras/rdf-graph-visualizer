b74ef96ae76a52ea8fd376b55327396e03d3f443
Wutras <wunsch.jonas@hotmail.com>
2021-12-07 15:26:55 +0100

    Add more functions from asd project
    Those functions can also be added with a few modifications. The last one missing is the
    expandNode functionality, but this requires a bit more work since the way nodes are loaded
    needs to be changed first.

--- src/helpers/loadGraph.js
+++ src/helpers/loadGraph.js
@@ -38,7 +38,7 @@ export function loadGraph(
     z: 1,
   };
 
-  const { nodes, links } = convertSparqlResultsToD3Graph({
+  let { nodes, links } = convertSparqlResultsToD3Graph({
     sparqlResults: graphData,
     prefixes,
     margin,
@@ -53,9 +53,9 @@ export function loadGraph(
 
   // #CONTAINERS
   //* container groups ensuring correct rendering order and therefore correct layering
-  const linkG = svg.append("g");
-  const nodeG = svg.append("g");
-  const linkTextG = svg
+  let linkG = svg.append("g");
+  let nodeG = svg.append("g");
+  let linkTextG = svg
     .append("g")
     .selectAll("g")
     .data(links)
@@ -64,7 +64,7 @@ export function loadGraph(
 
   // #NODES
   //* The node elements and their cosmetic attachments
-  const node = nodeG
+  let node = nodeG
     .attr("stroke", "#fff")
     .attr("stroke-width", "0.1px")
     .selectAll("g")
@@ -74,6 +74,7 @@ export function loadGraph(
     .on("click", (d) => {
       if (isDragging) return;
       showInfo({ type: d.rdfType, value: d.rdfValue, label: d.rdfsLabel });
+      toggleNode(d);
       d.isHighlightedFixed = !d.isHighlightedFixed;
     })
     .on("mouseover", (d) => {
@@ -85,9 +86,9 @@ export function loadGraph(
       if (!isDragging) updateOnce();
     });
 
-  const rectangle = node.append("rect").attr("fill", getColour);
+  let rectangle = node.append("rect").attr("fill", getColour);
 
-  const nodeText = node.append("text").text(getNodeText);
+  let nodeText = node.append("text").text(getNodeText);
 
   node
     .append("title")
@@ -98,7 +99,7 @@ export function loadGraph(
 
   // #LINKS
   //* Link elements and their cosmetic attachments
-  const link = linkG
+  let link = linkG
     .attr("stroke", "#999")
     .attr("stroke-width", "2px")
     .attr("stroke-opacity", "0.6")
@@ -107,9 +108,9 @@ export function loadGraph(
     .enter()
     .append("g");
 
-  const linkLine = link.append("line").attr("color", edgeColours.line);
+  let linkLine = link.append("line").attr("color", edgeColours.line);
 
-  const linkTextRect = linkTextG
+  let linkTextRect = linkTextG
     .append("rect")
     .attr("x", (d) => d.width - getNodeText(d).length)
     .attr("width", getLinkTextBoxWidth)
@@ -123,7 +124,7 @@ export function loadGraph(
     )
     .attr("fill", edgeColours.textBox);
 
-  const linkText = linkTextG
+  let linkText = linkTextG
     .append("text")
     .text(getNodeText)
     .attr("y", minNodeRadius / 1.5)
@@ -430,8 +431,14 @@ export function loadGraph(
 
   function boxingForce() {
     for (let n of nodes) {
-      n.x = Math.max(0, Math.min((width - (n.width ?? 0)) * Math.max(1, zoomOffset.z), n.x));
-      n.y = Math.max(0, Math.min((height - (n.height ?? 0)) * Math.max(1, zoomOffset.z), n.y));
+      n.x = Math.max(
+        0,
+        Math.min((width - (n.width ?? 0)) * Math.max(1, zoomOffset.z), n.x)
+      );
+      n.y = Math.max(
+        0,
+        Math.min((height - (n.height ?? 0)) * Math.max(1, zoomOffset.z), n.y)
+      );
     }
   }
 
@@ -501,18 +508,139 @@ export function loadGraph(
         visited.push(linkedNode);
         const furtherNodes = getAllLinkedNodes(linkedNode);
         furtherNodes.forEach(hideNodes);
-        const positionInData = graphData.nodes.indexOf(linkedNode);
-        const [removedNode] = graphData.nodes.splice(positionInData, 1);
-        hidden[sourceNode.company_id]?.nodes?.push?.(removedNode);
-        graphData.links = graphData.links.filter((edge) => {
+        const positionInData = nodes.indexOf(linkedNode);
+        const [removedNode] = nodes.splice(positionInData, 1);
+        hidden[sourceNode.id]?.nodes?.push?.(removedNode);
+        links = links.filter((edge) => {
           const isAssociated =
             edge.source !== linkedNode && edge.target !== linkedNode;
-          if (!isAssociated) hidden[sourceNode.company_id]?.links?.push?.(edge);
+          if (!isAssociated) hidden[sourceNode.id]?.links?.push?.(edge);
           return isAssociated;
         });
       }
     }
 
     linkedNodes.forEach(hideNodes);
-  }  
+  }
+
+  function drawGraph() {
+    while (svgElement.lastChild) {
+      svgElement.removeChild(svgElement.lastChild);
+    }
+    node.exit().remove();
+    link.exit().remove();
+    simulation.stop();
+    simulation.nodes(nodes).restart();
+    link.links = links;
+
+    // #CONTAINERS
+    //* container groups ensuring correct rendering order and therefore correct layering
+    linkG = svg.append("g");
+    nodeG = svg.append("g");
+    linkTextG = svg.append("g").selectAll("g").data(links).enter().append("g");
+
+    // #NODES
+    //* The node elements and their cosmetic attachments
+    node = nodeG
+      .attr("stroke", "#fff")
+      .attr("stroke-width", "0.1px")
+      .selectAll("g")
+      .data(nodes)
+      .enter()
+      .append("g")
+      .on("click", (d) => {
+        if (isDragging) return;
+        showInfo({ type: d.rdfType, value: d.rdfValue, label: d.rdfsLabel });
+        d.isHighlightedFixed = !d.isHighlightedFixed;
+      })
+      .on("mouseover", (d) => {
+        d.isHighlighted = true;
+        if (!isDragging) updateOnce();
+      })
+      .on("mouseout", (d) => {
+        d.isHighlighted = false;
+        if (!isDragging) updateOnce();
+      });
+
+    rectangle = node.append("rect").attr("fill", getColour);
+
+    nodeText = node.append("text").text(getNodeText);
+
+    node
+      .append("title")
+      .text(
+        (d) =>
+          `type: ${d.rdfType}, value: ${d.rdfValue}, linkCount: ${d.linkCount}`
+      );
+
+    // #LINKS
+    //* Link elements and their cosmetic attachments
+    link = linkG
+      .attr("stroke", "#999")
+      .attr("stroke-width", "2px")
+      .attr("stroke-opacity", "0.6")
+      .selectAll("line")
+      .data(links)
+      .enter()
+      .append("g");
+
+    linkLine = link.append("line").attr("color", edgeColours.line);
+
+    linkTextRect = linkTextG
+      .append("rect")
+      .attr("x", (d) => d.width - getNodeText(d).length)
+      .attr("width", getLinkTextBoxWidth)
+      .attr(
+        "height",
+        (d) =>
+          Math.max(
+            nodeRadiusFactor * zoomOffset.z + margin + padding,
+            minNodeRadius
+          ) - margin
+      )
+      .attr("fill", edgeColours.textBox);
+
+    linkText = linkTextG
+      .append("text")
+      .text(getNodeText)
+      .attr("y", minNodeRadius / 1.5)
+      .attr("x", (d) => d.width - getNodeText(d).length);
+
+    //* The arrow heads indicating the direction of the edges
+    svg
+      .append("defs")
+      .append("marker")
+      .attr("id", "arrowhead")
+      .attr("viewBox", "-0 -5 10 10")
+      .attr("refX", 35)
+      .attr("refY", 0)
+      .attr("orient", "auto")
+      .attr("markerWidth", 13)
+      .attr("markerHeight", 13)
+      .attr("xoverflow", "visible")
+      .append("svg:path")
+      .attr("d", "M 0,-5 L 10 ,0 L 0,5")
+      .attr("fill", edgeColours.arrowHead)
+      .style("stroke", "#F00");
+  }
+
+  function collapseNode(nodeToCollapse) {
+    nodeToCollapse._isCollapsed = true;
+    hidden[nodeToCollapse.id] = {
+      nodes: [],
+      links: [],
+    };
+    hideDependentNodes(nodeToCollapse);
+    drawGraph();
+  }
+
+  
+
+  function toggleNode(nodeToToggle) {
+    if (nodeToToggle._isCollapsed) {
+      //expandNode(nodeToToggle);
+    } else {
+      collapseNode(nodeToToggle);
+    }
+  }
 }
